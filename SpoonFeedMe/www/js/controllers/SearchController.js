47521ce9042d4a26e8b7ef1fac884174740a0956
Laura Tammpere <laura.tammpere@gmail.com>
2015-10-24 13:51:28 -0700

    progress

--- SpoonFeedMe/www/js/controllers/SearchController.js
+++ SpoonFeedMe/www/js/controllers/SearchController.js
@@ -1,4 +1,15 @@
-app.controller('MainController', ['$scope', function($scope, $http) {
+app.controller('SearchController', ['$scope','$http',function($scope, $http) {
 
+	// 45.55.223.121
 
+	$scope.txt = "Nothing!";
+
+	$scope.searchRecipes = function() {
+		$http.get("http://45.55.223.121/customers").then(function(resp) {
+			console.log('Success',resp.data);
+			$scope.txt = resp.data;
+		}, function(err) {
+			console.log('ERR',err.status);
+		})
+	}
 }]);
