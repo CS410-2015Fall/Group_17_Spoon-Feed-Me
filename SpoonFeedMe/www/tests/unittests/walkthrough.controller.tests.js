cea04c02f6f6410708cab212fca1a5fffa969d60
Laura Tammpere <laura.tammpere@gmail.com>
2015-11-27 15:45:49 -0800

    test updates

--- SpoonFeedMe/www/tests/unittests/walkthrough.controller.tests.js
+++ SpoonFeedMe/www/tests/unittests/walkthrough.controller.tests.js
@@ -36,6 +36,30 @@ describe('TestWalkthroughController', function() {
             expect(scope.recipe).toBe(payload[0]);
             expect(scope.currentStepNum).toEqual(1);
             expect(scope.maxStepNum).toEqual(payload[0].instructions.length);
+
+            expect(scope.recognition.not.toBe(null));
+        });
+    });
+
+    describe("when I call next/prev step", function() {
+        it("should increment the current step (as long as subsequent step is in bounds", function() {
+            expect(scope.currentStepNum).toEqual(1);
+            
+            // expect that it won't decrement, since it is at step 1
+            scope.prevStep();
+            expect(scope.currentStepNum).toEqual(1);
+            
+            // expect that it will increment
+            scope.nextStep();
+            expect(scope.currentStepNum).toEqual(2);
+
+            // expect that it won't increment, since it is at maxStepNum
+            scope.nextStep();
+            expect(scope.currentStepNum).toEqual(2);
+
+            // expect that it will decrement
+            scope.prevStep();
+            expect(scope.currentStepNum).toEqual(1);
         });
     });
 
