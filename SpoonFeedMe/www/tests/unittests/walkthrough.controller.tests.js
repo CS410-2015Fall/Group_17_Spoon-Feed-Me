0dce0ce70d3d998b7a9bdaa8fa83ffa743f731f0
Laura <laura.tammpere@gmail.com>
2015-11-27 18:26:25 -0800

    added tests for various voice inputs

--- SpoonFeedMe/www/tests/unittests/walkthrough.controller.tests.js
+++ SpoonFeedMe/www/tests/unittests/walkthrough.controller.tests.js
@@ -2,14 +2,25 @@ describe('TestWalkthroughController', function() {
     var scope,stateparams;
     var recipeServiceMock = {};
 
+    // Mock event objects
+    function MockEvent(heardValue){
+        this.heardValue = heardValue;
+        return {
+            results: [[{
+                transcript: this.heardValue
+            }]]
+        }
+    }
+
     // Mock voice recognition object
     var recognitionMock = {
         running: false,
-        start: function() {},
-        stop: function() {},
-        abort: function() {},
+        start: function() { this.running=true },
+        stop: function() { this.running=false },
+        abort: function() { this.running=false },
     };
 
+    // Mock recipe payload
     var payload = [{"name": "Mock Recipe", 
         "ingredients": ["First Ingredient", 
                         "Second Ingredient", 
@@ -37,7 +48,8 @@ describe('TestWalkthroughController', function() {
         $controller('WalkthroughCtrl', {
             $scope: scope, 
             $stateParams: stateparams,
-            RecipeService: recipeServiceMock});
+            RecipeService: recipeServiceMock
+        });
     }));
 
     describe("after initializaiton", function() {
@@ -46,8 +58,104 @@ describe('TestWalkthroughController', function() {
             expect(scope.recipe).toBe(payload[0]);
             expect(scope.currentStepNum).toEqual(1);
             expect(scope.maxStepNum).toEqual(payload[0].instructions.length);
+        });
+    });
 
-          //  expect(scope.recognition.not.toBe(null));
+    describe("test ionicView events", function() {
+        
+        it("before entering view, voice recognition is not running", function() {
+            // before entering the view, voice recognition is off
+            expect(scope.recognition.running).toBe(false);
+        });
+
+        it("before entering view, onresult is not defined", function() {
+            expect(scope.recognition.onresult).toBe(undefined);
+        });
+
+        it("after entering view, voice recognition should be running", function() {
+            //scope.nextStep=jasmine.createSpy();
+
+            scope.$emit('$ionicView.beforeEnter');
+            // We expect that the voice recognition will have
+            // started after entering the view
+            expect(scope.recognition.running).toBe(true);
+        });
+
+        it("after entering view, onresult is not undefined", function() {
+            expect(scope.recognition.onresult).not.toBe(undefined);
+        });
+
+        describe("test voice inputs", function() {
+
+            // Before each voice input test, replace 
+            // the recognition callback with function in
+            // the testing scope (no dollar sign)
+            //
+            // The reason for this is that it results in errors
+            // to leave it as $scope.handleVoiceInput, like it is
+            // set when $ionicView.beforeEnter event occurs. I suspect
+            // this is because $scope and scope are slightly different
+
+            beforeEach(function() {
+                scope.recognition.onresult = scope.handleVoiceInput;
+            });
+
+            it("calls nextStep when user says 'next'", function() {
+
+                spyOn(scope,'nextStep');
+                var mockEvent = new MockEvent('next');
+                scope.recognition.onresult(mockEvent);
+
+                // When recognition 'hears' next, call nextStep()
+                expect(scope.nextStep).toHaveBeenCalled();
+            });
+
+            it("calls prevStep when user says 'back' or 'previous", function() {
+
+                spyOn(scope,'prevStep');
+
+                // all commands which will return to previous step
+                var variants = ['back','previous'];
+
+                variants.forEach(function (variant) {
+                    var mockEvent = new MockEvent(variant);
+                    scope.recognition.onresult(mockEvent);
+                    expect(scope.prevStep).toHaveBeenCalled();
+                });
+            });
+
+            // TODO: Slower/faster
+            // maybe after adjusting the slower/faster to be
+            // a gradiant (e.g. so you can return to normal speed)
+
+            // TODO: stopping voice recognition?
+            it("calls voice upon certain commands", function() {
+
+                spyOn(scope,'voice');
+
+                // all commands which will cause the app to
+                // read out the current instruction using TTC
+                var variants = ['read','what','repeat'];
+
+                variants.forEach(function (variant) {
+                    var mockEvent = new MockEvent(variant);
+                    scope.recognition.onresult(mockEvent);
+                    expect(scope.voice).toHaveBeenCalled();
+
+                    // simulate the fact that the voice recognition
+                    // will restart after the voice function runs
+
+                    // TODO: restart voice at this point? 
+                    scope.recognition.start();
+                });
+            });
+
+        });
+
+        it("before leaving view, abort voice recognition", function() {
+            expect(scope.recognition.running).toBe(true);
+            scope.$emit('$ionicView.beforeLeave');
+            expect(scope.recognition.running).toBe(false);
         });
     });
 
